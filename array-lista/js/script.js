5fd7d392fb7a215ba195ccc6704d645832acb19d
DarioLoForti <lofortidario@gmail.com>
2023-11-24 13:35:39 +0100

    creo variabile frutta con valore pesca

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -2,3 +2,11 @@
 
 let frigo = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 
+console.log(frigo);
+
+// 2 - aggiungo la pesca all'arrey del frigo
+
+let frutta = "pesca";
+
+console.log(frutta);
+
