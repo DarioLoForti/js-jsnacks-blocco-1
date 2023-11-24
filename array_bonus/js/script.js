aa48284a6cb8337c0d871bb541c0937439b9dc41
DarioLoForti <lofortidario@gmail.com>
2023-11-24 15:15:27 +0100

    [array_bonus]creo 2 array con n° elementi diverso

---
+++ array_bonus/js/script.js
@@ -0,0 +1,11 @@
+// 1 - creo due array che hanno il numero di elementi diversi
+
+let pari = [2,4,6,8,10,12,14,16,18,20];
+console.log(pari);
+
+let dispari = [1,3,5,7,9,11];
+console.log(dispari);
+
+
+
+// 2 - Aggiungo elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
