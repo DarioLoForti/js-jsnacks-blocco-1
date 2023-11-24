4aae1a319a2051b79cff2fae6a79c6e36708be67
DarioLoForti <lofortidario@gmail.com>
2023-11-24 15:34:36 +0100

    aggiungo elementi mancanti nell'array minore

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -6,6 +6,18 @@ console.log(pari);
 let dispari = [1,3,5,7,9,11];
 console.log(dispari);
 
+let differenza = pari.length - dispari.length;
+console.log(differenza);
 
 
 // 2 - Aggiungo elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+
+for(let i=0; i<differenza; i++){
+
+    dispari.push(i);
+    
+}
+
+
+console.log(pari);
+console.log(dispari);
