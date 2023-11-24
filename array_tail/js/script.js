d55401f717004070f683c58d2eaff300a5976bd8
DarioLoForti <lofortidario@gmail.com>
2023-11-24 15:06:04 +0100

    [array_tail] Bonus

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,3 +1,29 @@
+// // 1 - chiedo all'utente il numero dei valori presenti nell'arrey
+
+// let numbArray =parseInt(prompt("inserisci il numero degli elementi dell'array"));
+// console.log(numbArray);
+
+// // 2 - genero numeri casuali (tra 1 e 100) tanti quanti il numero richiesto dall'utente
+
+// let array =[];
+
+// for(let i =0; i<=numbArray; i++){
+//     let elementRandom = Math.floor(Math.random() * 100) +1;
+//     console.log(elementRandom);
+//     array.push(elementRandom);
+// }
+// console.log(array);
+
+// // 3 - stampo in console gli ultimi 5 elementi dell'arrey
+
+// let lastFive = array.slice(-5);
+
+// console.log(lastFive);
+
+
+// BONUS
+
+
 // 1 - chiedo all'utente il numero dei valori presenti nell'arrey
 
 let numbArray =parseInt(prompt("inserisci il numero degli elementi dell'array"));
@@ -14,9 +40,12 @@ for(let i =0; i<=numbArray; i++){
 }
 console.log(array);
 
-// 3 - stampo in console gli ultimi 5 elementi dell'arrey
+// 3 - Chiedo all'utente quanti elemnti dell'array dovremmo stampare
 
-let lastFive = array.slice(-5);
+let numbArrayPrint = parseInt(prompt("inserisci il numero di elemneti dell'array da stampare"));
+
+// 3 - stampo in console il numero di elementi richiesti dall'utente
+
+let lastFive = array.slice(array, numbArrayPrint);
 
 console.log(lastFive);
-
